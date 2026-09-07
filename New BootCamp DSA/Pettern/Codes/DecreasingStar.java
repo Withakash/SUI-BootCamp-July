e9f4548d0c94353c663eada894a796304d4321c4
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-09-07 11:25:14 +0530

    Create DecreasingStar.java

---
+++ New BootCamp DSA/Pettern/Codes/DecreasingStar.java
@@ -0,0 +1,12 @@
+
+public class DecreasingStar {
+    public static void main(String[] args){
+        int n = 3;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = i ; j <= n ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    } 
+}
